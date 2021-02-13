ea9ab238eca6a57df64270f075cdc79e3eb35150
Kaito4869 <78203230+Kaito4869@users.noreply.github.com>
2021-02-13 17:17:54 +0900

    userモデル設計

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :comments
+  has_many :purchases
+
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :nickname, presence: true
+  validates :birth, presence: true
+end
