5e51c3c7806610be6ccefb9f6653c4eb090e82e8
Kaito4869 <78203230+Kaito4869@users.noreply.github.com>
2021-02-23 15:24:40 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items do
-<<<<<<< Updated upstream
     resources :orders, only: [:index, :create]
-=======
-    resources :purchases, only: [:index]
->>>>>>> Stashed changes
   end
 end
