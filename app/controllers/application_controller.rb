0bfdbbc8c4697b2e2ffb5784f020ffbc671959f0
Kaito4869 <78203230+Kaito4869@users.noreply.github.com>
2021-02-24 13:40:49 +0900

    インデント修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :nickname,
-                                             :birth_date])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :nickname, :birth_date])
   end
 
   def basic_auth
